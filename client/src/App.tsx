302fe30333dc40c9edcbdd01743998952fd13137
Vivek Patel <vivek.patel@eedeecreations.com>
2025-01-01 22:58:51 +0530

    Production error fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ const App: React.FC = () => {
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
   return (
     <GoogleOAuthProvider clientId={clientId}>
-    <Router>
+    <Router basename='/'>
       <AppContent />
     </Router>
     </GoogleOAuthProvider>
