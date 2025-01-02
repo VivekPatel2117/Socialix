843d766bbd3479ff25505c0f56f490ff8e7a2754
Vivek Patel <vivek.patel@eedeecreations.com>
2025-01-02 18:18:16 +0530

    Added routerConfig

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,14 @@ import client from "./apollo/client";
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
+import routerConfig from './react-router.config'
 // Get the root element
 const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = createRoot(rootElement);
 
   root.render(
-    <BrowserRouter>
+    <BrowserRouter {...(routerConfig)}>
         <ApolloProvider client={client}>
           <App />
         </ApolloProvider>
---
+++ client/src/react-router.config.ts
@@ -0,0 +1,7 @@
+import { type Config } from "@react-router/dev/config";
+
+const routerConfig = {
+  ssr: false,
+} satisfies Config;
+
+export default routerConfig;
