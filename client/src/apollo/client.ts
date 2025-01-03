29febd56896ee49a711a85f3f0579fdcee8b1565
Vivek Patel <vivek.patel@eedeecreations.com>
2025-01-03 23:17:07 +0530

    Added explore page

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://socialix.onrender.com',
+  uri: 'https://socialix.onrender.com',
 });
 
 const authLink = setContext((_, { headers }) => {
